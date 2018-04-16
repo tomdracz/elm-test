761cde676c424caf24f82dc039f4d7ef6a14354d
Tom Dracz <tjdracz@gmail.com>
2018-04-16 21:21:59 +0100

    Play around with union types

--- Hello.elm
+++ Hello.elm
@@ -8,13 +8,29 @@ square n =
     n ^ 2
 
 
+type Meaning
+    = OfLife
+    | OtherMeaning
+
+
 meaning : Int
 meaning =
     square 7 - 7
 
 
+meaningOfLife : Meaning -> Int
+meaningOfLife answer =
+    case answer of
+        OfLife ->
+            meaning
+
+        OtherMeaning ->
+            41
+
+
 main =
-    meaning
+    OfLife
+        |> meaningOfLife
         |> toString
         |> (++) "The meaning of life is: "
         |> text
