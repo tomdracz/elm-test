7e161610328051035e2a194b3e4e46c714496117
Tom Dracz <tjdracz@gmail.com>
2018-04-16 21:04:48 +0100

    Run elm format

--- Hello.elm
+++ Hello.elm
@@ -2,16 +2,19 @@ module Hello exposing (..)
 
 import Html exposing (text)
 
+
 square : Int -> Int
 square n =
-    n^2
+    n ^ 2
+
 
 meaning : Int
 meaning =
     square 7 - 7
 
+
 main =
     meaning
         |> toString
         |> (++) "The meaning of life is: "
-        |>  text
+        |> text
